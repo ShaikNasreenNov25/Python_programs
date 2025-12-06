d23ee4343f099fd643e8cb2d8a279dc287c312e6
ShaikNasreenNov25 <khaleelnasreen687@gmail.com>
2025-12-06 13:46:24 +0530

    Assignment 1

---
+++ assignment_1.py
@@ -0,0 +1,21 @@
+# Task 1: Perform Basic Mathematical Operations
+first = int(input("Enter the first number: "))
+second = int(input("Enter the second number: "))
+
+add = first + second
+sub = first - second
+mul = first * second
+div = first / second
+
+print("\nAddition:",add)
+print("Subtraction:",sub)
+print("Multiplication:",mul)
+print("Division:",div)
+
+
+# Task 2: Create a personalized Greeting
+name1 = input("\n\nEnter your first name: ")
+name2 = input("Enter your second name: ")
+
+print(f"\nHello, {name1} {name2}! Welcome to the Python program.")
+
